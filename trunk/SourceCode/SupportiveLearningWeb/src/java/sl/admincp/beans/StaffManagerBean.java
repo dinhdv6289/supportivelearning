d7998111e85f0f1f96d6e1443d414e9e48d8b34a
phantuyenhy@gmail.com <phantuyenhy@gmail.com@2ab22888-6ae4-8bd2-69d2-6341b5893142>
2011-04-01 18:51:55 +0000

    dao

--- trunk/SourceCode/SupportiveLearningWeb/src/java/sl/admincp/beans/StaffManagerBean.java
+++ trunk/SourceCode/SupportiveLearningWeb/src/java/sl/admincp/beans/StaffManagerBean.java
@@ -5,14 +5,17 @@
 package sl.admincp.beans;
 
 import el.dao.StaffDAO;
+import el.model.Batch;
 import el.model.Staff;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import org.primefaces.event.SelectEvent;
 
 /**
  *
@@ -24,11 +27,58 @@ public class StaffManagerBean implements Serializable {
 
     private StaffDAO staffDAO = new StaffDAO();
     private Staff staff = new Staff();
+    private Staff selectedstaff = new Staff();
     private ArrayList<Staff> listStaffs = new ArrayList<Staff>();
     private Staff[] selectedStaffs;
     private static boolean panelGroupListStaffs;
     private static final String REDIRECT = "?faces-redirect=true";
     private static final String THISPAGE = "staffManager.jsf";
+    private Batch batch = new Batch();
+
+    public Batch getBatch() {
+        return batch;
+    }
+
+    public Staff getSelectedstaff() {
+        return selectedstaff;
+    }
+
+    public void setSelectedstaff(Staff selectedstaff) {
+        this.selectedstaff = selectedstaff;
+    }
+
+    public void setBatch(Batch batch) {
+        this.batch = batch;
+    }
+
+    public String listBatch(int staffId) {
+        String result = "";
+        try {
+            ArrayList<Batch> batchs = staffDAO.getListBatchsByStaffId(staffId);
+            for (Batch b : batchs) {
+                result += b.getName() + ", ";
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(StaffManagerBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        if (result.length() > 2) {
+            result = result.substring(0, result.length() - 2);
+        }
+        return result;
+    }
+
+    public List<Batch> listBatchsOfstaff(int staffId) {
+        ArrayList<Batch> batchs = new ArrayList<Batch>();
+        StaffDAO dao = new StaffDAO();
+        try {
+
+            batchs = dao.getListBatchsByStaffId(staffId);
+        } catch (Exception ex) {
+            Logger.getLogger(StaffManagerBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return batchs;
+    }
 
     /** Creates a new instance of StaffManagerBean */
     public StaffManagerBean() {
@@ -85,5 +135,29 @@ public class StaffManagerBean implements Serializable {
         } catch (Exception ex) {
             Logger.getLogger(StaffManagerBean.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
+
+    public String deleteBatchByStaff() {
+        try {
+
+            boolean deleteBatchByStaff = staffDAO.deleteBatchByStaff(selectedstaff, batch);
+        } catch (Exception ex) {
+            Logger.getLogger(StaffManagerBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return "";
+    }
+
+    public String insertStaffAndBatch() {
+        try {
+            staffDAO.insertStaffAndBatch(selectedstaff, batch);
+        } catch (Exception ex) {
+            Logger.getLogger(StaffManagerBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return "";
+    }
+
+    public String details(SelectEvent event) {
+
+        return "staffdetails" + REDIRECT;
     }
 }
