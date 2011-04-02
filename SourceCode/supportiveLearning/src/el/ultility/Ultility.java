295b7bc6689612364cbf732fc23e7c542e3dc38c
phantuyenhy@gmail.com <phantuyenhy@gmail.com@2ab22888-6ae4-8bd2-69d2-6341b5893142>
2011-04-02 13:21:04 +0000

    dao

---
+++ SourceCode/supportiveLearning/src/el/ultility/Ultility.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package el.ultility;
+
+import java.util.Date;
+
+/**
+ *
+ * @author TuyenPV
+ */
+public class Ultility {
+
+    public static String GenerateUserName(String str) {
+       String[] s = str.split(" ");
+        String b = "";
+        String name = s[s.length - 1];
+        for (int i = 0; i < s.length; i++) {
+            if (i < s.length - 1) {
+                b += s[i].substring(0, 1);
+            }
+        }
+        String result = name + b;
+        return result.toLowerCase();
+    }
+
+    public static Date sql2date(java.sql.Date date) {
+        if (date == null) {
+            return null;
+        }
+        return new java.util.Date(date.getTime());
+    }
+
+    public static java.sql.Date date2sql(Date date) {
+        if (date == null) {
+            return null;
+        }
+        return new java.sql.Date(date.getTime());
+    }
+ 
+}
